f8e8cab3a3e11fc32a3e647c906a2373b7215451
RaheeqMousa <raheeqmousa00@gmail.com>
2024-10-17 21:58:46 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,16 +18,16 @@ document.querySelector(".SwitchMode").onclick=changeLight;
 /*.....................................................Delete the Notification Bar......................................................*/
 
 function DeleteNotification(){
-    document.querySelector(".NotificationBar").remove();
+    document.querySelector(".NotificationBar").remove();  //selects the notification bar div and remove it
 }
 
-document.querySelector(".DeleteNotificationButton").onclick=DeleteNotification;
+document.querySelector(".DeleteNotificationButton").onclick=DeleteNotification; //if user clicked on the delete button i will delete the notofication
 
 /*..................................................... GET AND DISPLAY CATEGORIES .....................................................*/
 
 const getCategories = async () => {     /* THIS FUNCTION RETURN THE CATEGORIES WE GOT FROM THE API */
     const {data} = await axios.get('https://dummyjson.com/products/category-list');
-    /* I WROTE {data} SO THE THE DATA WE GOT BE STORED INSIDE AN ARRAY */
+    // I WROTE {data} SO THE THE DATA WE GOT BE STORED INSIDE AN ARRAY
     return data;
 }
 
@@ -44,7 +44,6 @@ const displayCategoryList= async()=>{   /* THIS DISPLAYS THE CATEGORIES TO THE H
 
         const result=categories.map(function(category){
         
-            console.log(category);
             return `
             <div class="category">
                 <h2>${category}</h2>
@@ -52,7 +51,7 @@ const displayCategoryList= async()=>{   /* THIS DISPLAYS THE CATEGORIES TO THE H
             </div> 
             `;
     
-        }).join('');  /* THE DATA WE GOT IS DISPLAYED AND THERE IS ',' BETWEEN THEM SO WE JOIN THE DATA WITH '' (EMPTY QUOTES) */
+        }).join('');  // THE DATA WE GOT IS DISPLAYED AND THERE IS ',' BETWEEN THEM SO WE JOIN THE DATA WITH '' (EMPTY QUOTES)
     
         document.querySelector('.categories .row').innerHTML = result;
         
@@ -72,44 +71,84 @@ displayCategoryList();
 
 /*..................................................... GET AND DISPLAY PRODUCTS .....................................................*/
 
-
-const getProducts = async() =>{     /* THIS FUNCTION RETURN ALL THE PRODUCTS WE GOT FROM THE API */
-    const {data} = await axios.get('https://dummyjson.com/products');
+const getTotalNumberOfProducts = async() =>{     /* THIS FUNCTION RETURN THE TOTAL NUMBER OF PRODUCTS */
+    
+    const {data} = await axios.get(`https://dummyjson.com/products`);
     /* I WROTE {data} SO THE THE DATA WE GOT BE STORED INSIDE AN ARRAY */
 
-    return data
+    return data.total;
 
 }
 
-const displayProductList= async()=>{    /* THIS FUNCTION DISPLAYS ALL THE PRODUCTS WE GOT FROM THE API TO THE HTML FILE */
+const getProducts = async(page) =>{     /* THIS FUNCTION RETURN ALL THE PRODUCTS WE GOT FROM THE API */
+    
+    const skip=(page-1)*10;
+    const {data} = await axios.get(`https://dummyjson.com/products?limit=10&skip=${skip}`);
+    // I WROTE {data} SO THE THE DATA WE GOT BE STORED INSIDE AN ARRAY
+
+    return data.products;
+
+}
+
+const displayProductList= async(page = 1)=>{    /* THIS FUNCTION DISPLAYS ALL THE PRODUCTS WE GOT FROM THE API TO THE HTML FILE */
+
     document.querySelector('.loader').classList.remove("stop-loader");
     document.querySelector('.loader-container').classList.remove("stop-loader");
     const foot = document.querySelector('footer');
     foot.style.marginTop='250px';
 
     try{
-    const products=await getProducts();
-    const result= products.products.map(function(p){
+        const products=(await getProducts(page));
+        // I want to display 10 products in the page (the limit of products is 10 ) 
+        const numberOfPages=Math.ceil((await getTotalNumberOfProducts())/10); 
 
-        return `
-            <div class="product">
-                <img src="${p.thumbnail}" alt="this is product img"></img>
-                <h3>${p.title}</h3>
-            </div> 
-        `;
+        const result= products.map(function(p){
+            return `
+                <div class="product">
+                    <img src="${p.thumbnail}" alt="this is product img"></img>
+                    <h3>${p.title}</h3>
+                </div> 
+            `;
 
-    }
-    ).join('');
+        }  ).join('');
 
-        document.querySelector('.products .row').innerHTML=result;
+            document.querySelector('.products .container .row').innerHTML=result;
+
+            /* This part of code to display the pagination to the page under the products */
+            let paginationLink=``;
+
+            if(page==1){
+                // If the user in the first page the previous button must be disabled 
+                paginationLink=`<button class="page-link" onClick=displayProductList('${page-1}') disabled>&laquo</button>`;
+            }else{
+                paginationLink=`<button class="page-link" onClick=displayProductList('${page-1}') >&laquo</button>`;
+            }
+
+            for(let i=1;i<=numberOfPages;i++){
+                //if the page 1 is selected then the background of the button whice have the number on it must change its background
+                // when the button is clicked I will show the data for the specific page number that clicked on 
+                paginationLink+=`<button  class="page-link ${i==page?'active-page':''}" onClick=displayProductList('${i}')>${i}</button>`; 
+            }
+
+            if(page==numberOfPages){
+                // If the user in the last page the next button must be disabled 
+                paginationLink += `<button  class="page-link" onClick=displayProductList('${parseInt(page)+1}') disabled>&raquo</button>`;
+            }else{
+                paginationLink+=`<button class="page-link" onClick=displayProductList('${parseInt(page)+1}') >&raquo</button>`;
+            }
+
+            
+            document.querySelector(".pagination-container").innerHTML = paginationLink;
+            
     }catch(e){
-        document.querySelector('.products .row').innerHTML=`<p>error with products</p>`
+        document.querySelector('.products .container .row').innerHTML=`<p>${e}</p>`
     }finally{
         document.querySelector('.loader').classList.add("stop-loader");
         document.querySelector('.loader-container').classList.add("stop-loader");
         const foot = document.querySelector('footer');
         foot.style.marginTop='0px';
     }
+
 
 }
 displayProductList();
